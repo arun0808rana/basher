6b58369c6bbd92f5a2c35161a7988e2c4240f35b
Live session user <arun0808rana@gmail.com>
2022-01-26 07:38:12 +0000

    improv

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-sudo apt-get update
-
-sudo snap install chromium
-
-sudo snap install code --classic
-
-sudo snap install node --classic
-
-sudo apt install git
+yes | sudo snap install chromium
+yes | sudo snap install code --classic
+yes | sudo snap install node --classic
+yes | sudo apt-get update
+yes | sudo apt install git
+yes | sudo npm install -g yarn
+yes | sudo snap install blender --classic
+git config --global user.email "arun0808rana@gmail.com"
--- basher.sh
+++ basher.sh
@@ -1,6 +1,8 @@
-sudo snap install chromium
-sudo snap install code --classic
-sudo snap install node --classic
-sudo apt-get update
-sudo apt install git
-sudo npm install -g yarn
+yes | sudo snap install chromium
+yes | sudo snap install code --classic
+yes | sudo snap install node --classic
+yes | sudo apt-get update
+yes | sudo apt install git
+yes | sudo npm install -g yarn
+yes | sudo snap install blender --classic
+git config --global user.email "arun0808rana@gmail.com"
