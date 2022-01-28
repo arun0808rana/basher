191d4ed5e9cd289c26741ecf1eb3a854dc603fd1
Live session user <arun0808rana@gmail.com>
2022-01-28 08:06:01 +0000

    improv

--- basher.sh
+++ basher.sh
@@ -1,11 +1,11 @@
-yes | sudo snap install chromium
-yes | sudo snap install code --classic
-yes | sudo snap install node --classic
 yes | sudo apt-get update
 yes | sudo apt install git
 yes | sudo npm install -g yarn
-yes | sudo snap install blender --classic
+yes | sudo snap install node --classic
+yes | sudo snap install code --classic
 git config --global user.email "arun0808rana@gmail.com"
+https://github.com/arun0808rana/basher.git
 https://github.com/arun0808rana/vocab-better.git
 https://github.com/arun0808rana/vocab-better-backend.git
-https://github.com/arun0808rana/basher.git
+yes | sudo snap install chromium
+yes | sudo snap install blender --classic
