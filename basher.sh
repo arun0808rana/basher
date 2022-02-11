bd136924bacca4eec3bff3909c4616a59fbb6c40
Arun Rana <arun0808rana@gmail.com>
2022-02-12 02:37:57 +0530

    Update basher.sh

--- basher.sh
+++ basher.sh
@@ -5,7 +5,7 @@ yes | curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 yes | sudo apt-get install -y nodejs
 yes | sudo apt install git
 yes | sudo npm install -g yarn
-yes | sudo snap install node --classic
+# yes | sudo snap install node --classic
 yes | sudo snap install code --classic
 git config --global user.email "arun0808rana@gmail.com"
 git clone https://github.com/arun0808rana/basher.git
